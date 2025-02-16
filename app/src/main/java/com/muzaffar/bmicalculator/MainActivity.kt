edab2cd38d3d8cdaa1c7baac8c6f20ab4f291d09
Wan Muzaffar <wanmuz86@gmail.com>
2025-02-16 12:25:45 +0800

    add snackbar

--- app/src/main/java/com/muzaffar/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/muzaffar/bmicalculator/MainActivity.kt
@@ -16,8 +16,12 @@ import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -33,6 +37,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.muzaffar.bmicalculator.ui.theme.BMICalculatorTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -47,11 +52,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BMICalculatorScreen (){
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
     @OptIn(ExperimentalMaterial3Api::class)
     BMICalculatorTheme {
-        Scaffold(modifier = Modifier.fillMaxSize(),
+        Scaffold(
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+            modifier = Modifier.fillMaxSize(),
             topBar = {
                 TopAppBar(title = { Text(text = "BMI Calculator")}   )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {
+                    // Show snackbar in another suspend (async-await)
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar("Welcome to my app")
+                    }
+                }) {
+                    Icon(painter =painterResource(R.drawable.ic_launcher_foreground),
+                        contentDescription = "About me")
+                }
             }
         ) { padding->
             BMICalculator(modifier = Modifier.padding(padding))
@@ -60,13 +81,17 @@ fun BMICalculatorScreen (){
 }
 
 
+
+
 @Composable
 fun BMICalculator(modifier: Modifier = Modifier){
+
     var weight by remember { mutableFloatStateOf(70f) }
     var height by remember { mutableFloatStateOf(170f) }
 
     val bmi = weight / (height/100 * height/100) // weight (kg) / (height in m)^2
     val bmiCategory = calculateBMI(bmi)
+
 
     Column(modifier = modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally){
@@ -95,11 +120,13 @@ fun BMICalculator(modifier: Modifier = Modifier){
             // inside material library
             shape = RoundedCornerShape(8.dp),
             colors = CardDefaults.cardColors(containerColor = Color(0xFFE3F2FD)),
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp),
+            elevation = CardDefaults.cardElevation(8.dp)
         ){
             Column(
                 modifier = Modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
+
             ) {
                 Text(text = "Your BMI is ${String.format("%.2f", bmi)}",
                     fontSize = 24.sp)
